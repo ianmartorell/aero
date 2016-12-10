78867340e9cc7a6f7ca2d26f84fcec83849b14b6
Ian Martorell <ianmartorell@gmail.com>
2016-12-10 06:40:11 +0100

    Removed extra argument in DVM call

--- flat_plate_test.m
+++ flat_plate_test.m
@@ -1,5 +1,5 @@
 camberLine = airfoil('0112', 1, 1, 0, 5, 'linear');
-circulation = DVM(camberLine, 1, 5, 5);
+circulation = DVM(camberLine, 1, 5);
 circulation = circulation/sind(5)*5/pi;
 disp('Γ/(π*∆c*U*sin(a))=');
 disp(circulation);
--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 camberLine = airfoil('2212', 1, 0.8, 5, 100, 'linear');
 plot(camberLine(1), camberLine(2));
 axis equal;
-circulation = DVM(camberLine, 1, 5, 5);
+circulation = DVM(camberLine, 1, 5);
 Cl = (2/1*1)*circulation;
 CL = sum(Cl);
 disp('CL');
