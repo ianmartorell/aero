9c4c76b49075edf3a85f366fb3c52b3c52370f66
Ian Martorell <ianmartorell@gmail.com>
2016-12-10 06:06:06 +0100

    Changed tabs to spaces

--- DVM.m
+++ DVM.m
@@ -16,25 +16,25 @@ influenceCoefficients = zeros(nPanels, nPanels);
 RHS = zeros(nPanels, 1);
 
 for i = 1:nPanels
-		% Find the unit normal vector at this control point
-		d = zeros(2, 1);
+    % Find the unit normal vector at this control point
+    d = zeros(2, 1);
     d(1) = (camberLine(1, i+1) - (camberLine(1, i)));
     d(2) = (camberLine(2, i+1) - (camberLine(2, i)));
-		unitTangentVector = zeros(2, 1);
+    unitTangentVector = zeros(2, 1);
     unitTangentVector(1) = d(1) / sqrt(d(1)^2 + d(2)^2);
-		unitTangentVector(2) = d(2) / sqrt(d(1)^2 + d(2)^2);
-		unitNormalVector = zeros(2, 1);
-		unitNormalVector(1) = - unitTangentVector(2);
-		unitNormalVector(2) = unitTangentVector(1);
+    unitTangentVector(2) = d(2) / sqrt(d(1)^2 + d(2)^2);
+    unitNormalVector = zeros(2, 1);
+    unitNormalVector(1) = - unitTangentVector(2);
+    unitNormalVector(2) = unitTangentVector(1);
     for j = 1:nPanels
-				% Compute the induced velocity at i due to a lumped vortex at j
+        % Compute the induced velocity at i due to a lumped vortex at j
         d(1) = controlPoints(1, i) - vortices(1, j);
         d(2) = controlPoints(2, i) - vortices(2, j);
         r = sqrt(d(1)^2 + d(2)^2);
-				inducedVelocity = zeros(2, 1);
+        inducedVelocity = zeros(2, 1);
         inducedVelocity(1) = 1/(2*pi) * d(2)/r^2;
         inducedVelocity(2) = -1/(2*pi) * d(1)/(r)^2;
-				% Compute the influence coefficients
+        % Compute the influence coefficients
         influenceCoefficients(i, j) = inducedVelocity(1) * unitNormalVector(1) + inducedVelocity(2) * unitNormalVector(2);
     end
     RHS(i) = -freestreamVelocity * ((cosd(angleOfAttack) * unitNormalVector(1)) + (sind(angleOfAttack) * unitNormalVector(2)));
